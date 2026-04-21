c0aa5658109f748f63a0b255f8cd6325372554cc
dharmildodiya <130409549+dharmildodiya@users.noreply.github.com>
2026-04-21 20:47:32 +0530

    Update notifier.py

--- notifier.py
+++ notifier.py
@@ -6,13 +6,20 @@ import pytz
 bot = Bot(token=BOT_TOKEN)
 
 async def send_unban(user_id, username, start_time):
-IST = pytz.timezone("Asia/Kolkata")
-now = datetime.datetime.now(IST)
+    IST = pytz.timezone("Asia/Kolkata")
+    now = datetime.datetime.now(IST)
+
     duration = now - start_time
 
-    message = f"""Account Recovered | @{username} 🏆✅
-⏱ Time Taken: {str(duration).split('.')[0]}
-Unbanned at {now.strftime('%Y-%m-%d %H:%M:%S IST')}
-https://instagram.com/{username}"""
+    message = f"""🏆 <b>ACCOUNT RECOVERED</b> ✅
 
-    await bot.send_message(user_id, message)
+👤 <b>@{username}</b>
+
+⏱ <b>Time Taken:</b> {str(duration).split('.')[0]}
+
+🕒 <b>Unbanned At:</b> {now.strftime('%I:%M %p | %d %b %Y')}
+
+🔗 https://instagram.com/{username}
+"""
+
+    await bot.send_message(user_id, message, parse_mode="HTML")
