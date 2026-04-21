b536d6d1f8241c723d8b6720efdf4bf7a0c9c376
dharmildodiya <130409549+dharmildodiya@users.noreply.github.com>
2026-04-21 20:27:25 +0530

    Update proxy_manager.py

--- proxy_manager.py
+++ proxy_manager.py
@@ -2,8 +2,13 @@ import random
 
 class ProxyManager:
     def __init__(self, file="proxies.txt"):
-        with open(file, "r") as f:
-            self.proxies = [line.strip() for line in f if line.strip()]
+        try:
+            with open(file, "r") as f:
+                self.proxies = [line.strip() for line in f if line.strip()]
+        except:
+            self.proxies = []
 
     def get_proxy(self):
+        if not self.proxies:
+            return None
         return random.choice(self.proxies)
